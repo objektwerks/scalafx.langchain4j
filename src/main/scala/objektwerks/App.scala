4611855b2fd9d91dabff434290f61a433c7fce91
objektwerks <objektwerks@runbox.com>
2025-06-19 17:19:10 -0400

    refactored app

--- src/main/scala/objektwerks/App.scala
+++ src/main/scala/objektwerks/App.scala
@@ -1,28 +1,8 @@
 package objektwerks
 
-import dev.langchain4j.model.openai.OpenAiChatModel
-
-import java.time.Duration
-
-@main def runApp(args: String*): Unit =
-  val apiKey = sys.env("OPENAI_API_KEY")
-  val modelName = "gpt-4o-mini"
-
-  val model = OpenAiChatModel
-    .builder()
-    .apiKey(apiKey)
-    .modelName(modelName)
-    .timeout(Duration.ofSeconds(30))
-    .logRequests(true)
-    .logResponses(true)
-    .build()
-  println(s"*** OpenAI Chat Model: $modelName")
-
-  val topic = if args.nonEmpty then args(0) else "Indian Pale Ale"
-  println(s"*** Topic: $topic")
-
-  val request = s"Tell me about this beer style, $topic"
-  println(s"*** Request: $request")
-
-  val response = model.chat(request)
-  println(s"*** Response: $response")
+@main def runApp: Unit =
+  val connector = Connector(
+    apiKey = sys.env("OPENAI_API_KEY"),
+    modelName = "gpt-4o-mini"
+  )
+  println(connector)
