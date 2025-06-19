4f7baffaae317cd38984785933ff550c1446ca6b
objektwerks <objektwerks@runbox.com>
2025-06-19 17:45:21 -0400

    enhanced context

--- src/main/scala/objektwerks/Context.scala
+++ src/main/scala/objektwerks/Context.scala
@@ -5,6 +5,8 @@ import com.typesafe.config.Config
 import scalafx.scene.image.Image
 
 final class Context(config: Config):
+  val connectorModelName = config.getString("connector.modelName")
+  
   val windowTitle = config.getString("window.title")
   val windowWidth = config.getDouble("window.width")
   val windowHeight = config.getDouble("window.height")
