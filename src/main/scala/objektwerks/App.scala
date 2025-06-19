2356600b2d20c30045afa12f2f44e0d62adc3703
objektwerks <objektwerks@runbox.com>
2025-06-19 17:22:37 -0400

    enhanced app

--- src/main/scala/objektwerks/App.scala
+++ src/main/scala/objektwerks/App.scala
@@ -1,8 +1,41 @@
 package objektwerks
 
-@main def runApp: Unit =
+import com.typesafe.config.ConfigFactory
+import com.typesafe.scalalogging.LazyLogging
+
+import java.awt.{Taskbar, Toolkit}
+import java.awt.Taskbar.Feature
+
+import scalafx.application.JFXApp3
+import scalafx.scene.image.Image
+
+object App extends JFXApp3 with LazyLogging:
+  logger.info("Starting app ...")
+
   val connector = Connector(
     apiKey = sys.env("OPENAI_API_KEY"),
     modelName = "gpt-4o-mini"
   )
   println(connector)
+
+  override def start(): Unit =
+    stage = new JFXApp3.PrimaryStage:
+      scene = View(context).scene
+      title = context.windowTitle
+      minWidth = context.windowWidth
+      minHeight = context.windowHeight
+      icons += context.imageAppIcon
+
+    if Taskbar.isTaskbarSupported() then
+      val taskbar = Taskbar.getTaskbar()
+      if taskbar.isSupported(Feature.ICON_IMAGE) then
+        val defaultToolkit = Toolkit.getDefaultToolkit()
+        val appIcon = defaultToolkit.getImage(getClass().getResource("/image/icon.png"))
+        taskbar.setIconImage(appIcon)
+
+    stage.show()
+
+    logger.info("Started app.")
+
+  sys.addShutdownHook:
+    logger.info("Shutdown app.")
