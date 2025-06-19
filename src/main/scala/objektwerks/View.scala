3e3fcf0e715ca7f5584a7f4a4ee5cbb838f91359
objektwerks <objektwerks@runbox.com>
2025-06-19 17:27:59 -0400

    added view

---
+++ src/main/scala/objektwerks/View.scala
@@ -0,0 +1,17 @@
+package objektwerks
+
+import scalafx.geometry.Insets
+import scalafx.scene.Scene
+import scalafx.scene.layout.VBox
+
+final class View(context: Context):
+  val menu = Menu(context)
+
+  val rootPane = new VBox:
+    spacing = 6
+    padding = Insets(6)
+    children = List(menu)
+
+  val scene = new Scene:
+    root = rootPane
+    stylesheets = List("/style.css")
