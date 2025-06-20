91ea0b7f9a7da5686ac046f175c121b286e3d4fd
objektwerks <objektwerks@runbox.com>
2025-06-20 11:49:37 -0400

    enhanced beer pane

--- src/main/scala/objektwerks/BeerPane.scala
+++ src/main/scala/objektwerks/BeerPane.scala
@@ -12,6 +12,7 @@ final class BeerPane(context: Context) extends VBox:
   val textBeer = new TextField():
     prefHeight = 25
     prefWidth = 100
+    promptText = "Enter a beer name or style ..."
     text = ""
 
   val labelSummary = new Label():
