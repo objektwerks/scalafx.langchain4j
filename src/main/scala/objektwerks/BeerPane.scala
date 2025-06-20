a2de96a4fd5474cced80bcc76e306a5bd54ad229
objektwerks <objektwerks@runbox.com>
2025-06-20 11:43:01 -0400

    enhanced beer pane

--- src/main/scala/objektwerks/BeerPane.scala
+++ src/main/scala/objektwerks/BeerPane.scala
@@ -1,5 +1,10 @@
 package objektwerks
 
+import scalafx.scene.control.Label
 import scalafx.scene.layout.VBox
 
-final class BeerPane(context: Context) extends VBox
+final class BeerPane(context: Context) extends VBox:
+  val labelBeer = new Label():
+    prefHeight = 25
+    prefWidth = 50
+    text = context.labelBeer
